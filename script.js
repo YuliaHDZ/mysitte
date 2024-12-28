05f9d2085b0aaece3e933516547ebeb65c94c452
YuliaHDZ <uliahoodzov@gmail.com>
2024-12-28 23:28:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("passwordForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Остановить стандартное поведение формы
+
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if (password === "secret") {
+        message.style.color = "green";
+        message.textContent = "Доступ разрешен! Добро пожаловать!";
+    } else {
+        message.style.color = "red";
+        message.textContent = "Неверный пароль. Попробуйте снова.";
+    }
+});
